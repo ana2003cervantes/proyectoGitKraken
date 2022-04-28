36de5b861e094a3bbf922b3c9c8bead5c16af085
ana2003cervantes <104142235+ana2003cervantes@users.noreply.github.com>
2022-04-28 18:52:09 +0200

    Add files via upload

---
+++ EjercicioClases6.java
@@ -0,0 +1,25 @@
+/*
+Programa que muestre los numeros impares comprendidos entre el 20 y el 40
+*/
+package com.mycompany.ejercicioclases6;
+
+import static com.mycompany.ejercicioclases6.ej6.esImpar;
+
+/**
+ *
+ * @author anacervantes
+ */
+public class EjercicioClases6 {
+    
+    public static void main(String[] args) {
+        for (int j=20;j<=40;j++) {
+            if (esImpar(j))
+            System.out.println((j));
+           
+}
+
+        
+    }
+    
+
+}
---
+++ ej6.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicioclases6;
+
+/**
+ *
+ * @author anacervantes
+ */
+public class ej6 {
+     public static boolean esImpar(int num) {
+    if (num%2!=0)
+        return true;
+    else
+        return false;
+}
+}
